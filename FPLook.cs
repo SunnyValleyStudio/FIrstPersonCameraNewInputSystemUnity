283dc8ca2a6112d3e21b6d8f80a3c2c57b3598fc
PiotrMa <piotrmgrupa4@gmail.com>
2020-02-09 08:28:05 +0100

    firt person camera tutorial with new input system

---
+++ FPLook.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class FPLook : MonoBehaviour
+{
+    [SerializeField]
+    private float mouseSensititiy = 5f;
+    [SerializeField]
+    private float headPitchAngleUpLimit = -20f, headPitchAngleDownLimit = 70f;
+
+    private Quaternion bodyOriginOrientation, headOriginOrientation;
+    private float currentYaw = 0f, currentPitch = 0f;
+
+    private float horizontalInput, verticalInput;
+
+    [SerializeField]
+    private Transform head;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HideCursor();
+    }
+
+    private void HideCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        bodyOriginOrientation = transform.localRotation;
+        headOriginOrientation = head.localRotation;
+    }
+
+    public void OnLookInput(InputAction.CallbackContext context)
+    {
+        var input = context.ReadValue<Vector2>();
+        horizontalInput = input.x;
+        verticalInput = input.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput *= Time.deltaTime * mouseSensititiy;
+        verticalInput *= Time.deltaTime * mouseSensititiy;
+
+        currentYaw += horizontalInput;
+        currentPitch += verticalInput;
+        currentPitch = Mathf.Clamp(currentPitch, headPitchAngleUpLimit, headPitchAngleDownLimit);
+    }
+
+    private void FixedUpdate()
+    {
+        var bodyRotation = Quaternion.AngleAxis(currentYaw, Vector3.up);
+        var headRotation = Quaternion.AngleAxis(-currentPitch, Vector3.right);
+
+        transform.localRotation = bodyRotation * bodyOriginOrientation;
+        head.localRotation = headRotation * headOriginOrientation;
+    }
+}
---
+++ FPMovement.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController))]
+public class FPMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float movementSpeed = 5f;
+    [SerializeField]
+    private float jumpHeight = 5f;
+    [SerializeField]
+    private float gravity = 20;
+
+    private Vector2 input;
+    private bool jumping = false;
+
+    private Animator animator;
+
+    private Vector3 movementDirtection = Vector3.zero;
+    private CharacterController controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+    }
+
+    public void OnMoveInput(InputAction.CallbackContext context)
+    {
+        input = context.ReadValue<Vector2>();
+    }
+
+    public void OnJumpInput(InputAction.CallbackContext context)
+    {
+        if(controller.isGrounded)
+            jumping = true;
+    }
+
+    private void FixedUpdate()
+    {
+        var localDIrection = new Vector3(input.x, 0, input.y) * movementSpeed;
+        var worldSpaceDirection = transform.TransformDirection(localDIrection);
+        if (controller.isGrounded)
+        {
+            animator.SetFloat("velocity", input.magnitude);
+            if (input.magnitude < 0.2f)
+            {
+                animator.SetFloat("velocity", 0);
+            }
+            if (jumping)
+            {
+                jumping = false;
+                worldSpaceDirection.y = jumpHeight;
+                if (localDIrection.magnitude > 0)
+                {
+                    animator.SetFloat("velocity", 0);
+                }
+            }
+            else
+            {
+                worldSpaceDirection.y = 0;
+            }
+            movementDirtection = worldSpaceDirection;
+        }
+        movementDirtection.y -= gravity * Time.deltaTime;
+
+        controller.Move(movementDirtection * Time.deltaTime);
+    }
+}
